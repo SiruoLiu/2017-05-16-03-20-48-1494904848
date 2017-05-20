a6ff7e9e90aaef2bfa3121c52fc67877f40c414b
SiruoLiu <1143398076@qq.com>
2017-05-20 17:13:46 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -5,7 +5,7 @@ const loadAllItems = require('./loadAllItems.js');
 //    return 'Hello World!';
 //};
 
-function printInventory(inputs) {
+modile.exports=function printInventory(inputs) {
 var item;
 var item_list=[];   
 var a=loadAllItems();
