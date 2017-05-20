ad02846a8974b3e6d793e08cb9c6fe92ec02debc
SiruoLiu <1143398076@qq.com>
2017-05-20 16:58:31 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -7,34 +7,33 @@ module.exports = function main() {
 
 function printInventory(inputs) {
 var item;
-var item_list=[];    
+var item_list=[];   
+var a=loadAllItems();
 var list='***<没钱赚商店>购物清单***';
 var sum=0;
-for(var i=0;i<inputs.length;i++){
- item=inputs[i];//
-    if(item_list.length===0){
-        item.count=1;
-        item_list.push(item);
-    }
-    else{for(var j=0;j<item_list.length;j++){
-        if(inputs[i]==item_list[j].barcode){
-           item_list[j].count++;
-        }
-        else if(j==item_list.length-1){
-         //  POS收银机此题运用了：  1 for循环遍历数组；
-             //                             2对象中键所对应的值；
-             //                             3拼接字符串；
-            item_list.push(item); item.count=0;
-    }
-   }
-  }  
- }
+
+
+for(var i=0;i<a.length;i++)
+{
+   item=a[i];
+   
+   item_list.push(item);
+    item_list[i].count=0;
+	for(var j=0;j<inputs.length;j++)
+	{
+		if(inputs[j]==a[i].barcode)
+		{
+			item_list[i].count++;
+		}
+	}
+}
+
 
     for(var n=0;n< item_list.length;n++){
-    
+    if(item_list[n].count!=0){
     list=list+'\n'+'名称:'+item_list[n].name+','+'数量:'+item_list[n].count+item_list[n].unit+','+'单价:'+item_list[n].price+'.00(元)'+','+'小计:'+item_list[n].count*item_list[n].price+'.00(元)';
      sum+=item_list[n].count*item_list[n].price;
-    }
+    }}
 
 list=list+'\n'+'----------------------'+'\n'+'总计:'+sum+'.00(元)'+'\n'+'**********************';
 
